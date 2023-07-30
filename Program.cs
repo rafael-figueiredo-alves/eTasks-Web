57c66bef319a5914cfa39990709aeebfc82a03ee
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-07-30 18:33:44 -0300

    Fazendo Testes de Class Library

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using eFirebase4CSharp.Classes;
+using eFirebase4CSharp.Interfaces;
 using eTasks_web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IeFirebase, eFirebase>();
 
 await builder.Build().RunAsync();
