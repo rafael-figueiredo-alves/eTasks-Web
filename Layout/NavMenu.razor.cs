36b673adca344d520c86bb4e7017e1d6c97fb2fc
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-01-12 20:51:00 -0300

    Update January 12, 2024
    => Created a new project with .Net 8

---
+++ Layout/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Layout
+{
+    public class NavMenuBase : ComponentBase
+    {
+    protected bool collapseNavMenu = true;
+
+    protected string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+    protected void ToggleNavMenu()
+    {
+        collapseNavMenu = !collapseNavMenu;
+    }
+}
+}
