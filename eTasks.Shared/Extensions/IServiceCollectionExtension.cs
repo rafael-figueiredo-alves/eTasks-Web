c6f000bff8dba5d69be685eec7f2174790c62c69
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-09-08 20:11:35 -0300

    Added eTranslate & eFirebase

--- eTasks.Shared/Extensions/IServiceCollectionExtension.cs
+++ eTasks.Shared/Extensions/IServiceCollectionExtension.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eFirebase4CSharp.Classes;
+using eFirebase4CSharp.Interfaces;
+using eTranslate;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eTasks.Shared.Extensions
 {
     public static class IServiceCollectionExtension
     {
+        private static string _baseURL = string.Empty;
+
         public static void LoadServices(this IServiceCollection Services, string BaseURL)
         {
-            Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(BaseURL) });
+            _baseURL = BaseURL;
+            Services.AddScoppedServices();
+            Services.AddSingletonServices();
+        }
+
+        private static void AddScoppedServices(this IServiceCollection Services)
+        {
+            Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(_baseURL) });
+            Services.AddScoped<IeFirebase, eFirebase>();
+        }
+
+        private static void AddSingletonServices(this IServiceCollection Services)
+        {
+            Services.AddeTranslateSingletonService(TranslationFileFullPath: _baseURL + "translation.json");
         }
     }
 }
--- eTasks/Program.cs
+++ eTasks/Program.cs
@@ -1,11 +1,12 @@
 using eTasks;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using eTasks.Shared.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.LoadServices(builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
