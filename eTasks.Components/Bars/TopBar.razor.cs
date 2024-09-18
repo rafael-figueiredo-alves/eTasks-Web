fe12a15428bcfa56718ce419d4462170f9f406c4
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-09-17 22:22:04 -0300

    Testing layout

---
+++ eTasks.Components/Bars/TopBar.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Bars
+{
+    /// <summary>
+    /// Implementação da barra superior
+    /// </summary>
+    public class TopBarBase: ComponentBase
+    {
+        [Parameter]
+        public string Height { get; set; } = "60px";
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
