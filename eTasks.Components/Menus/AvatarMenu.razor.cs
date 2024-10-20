a0a81b2651c0e83a868197b4af807afe124dd4a7
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-10-20 20:53:02 -0300

    Added Roboto font to project
    Added Roboto font;
    Created AvatarMenu, MainMenu and LanguageMenu code files

---
+++ eTasks.Components/Menus/AvatarMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Menus
+{
+    public class AvatarMenuBase : ComponentBase
+    {
+        [Parameter] public bool IsDarkMode { get; set; } = false;
+    }
+}
---
+++ eTasks.Components/Menus/LanguageMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Menus
+{
+    public class LanguageMenuBase : ComponentBase
+    {
+        [Parameter] public bool IsDarkMode { get; set; } = false;
+    }
+}
