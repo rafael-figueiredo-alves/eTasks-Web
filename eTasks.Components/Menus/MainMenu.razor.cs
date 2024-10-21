57e748ac61ca59a7a0be83479b1dd6022563724e
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-10-20 21:50:02 -0300

    Working on Menus

--- eTasks.Components/Menus/MainMenu.razor.cs
+++ eTasks.Components/Menus/MainMenu.razor.cs
@@ -5,5 +5,15 @@ namespace eTasks.Components.Menus
     public class MainMenuBase : ComponentBase
     {
         [Parameter] public bool IsDarkMode { get; set; } = false;
+
+        protected string CloseButton { get; set; } = "assets/UI/offcanvas/light/Close.png";
+
+        protected override void OnParametersSet()
+        {
+            if (IsDarkMode)
+                CloseButton = "assets/UI/offcanvas/dark/Close.png";
+            else
+                CloseButton = "assets/UI/offcanvas/light/Close.png";
+        }
     }
 }
