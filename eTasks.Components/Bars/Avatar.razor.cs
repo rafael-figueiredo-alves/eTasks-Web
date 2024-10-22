7fd0f2f8125addeeb6e9b5fb92811921b7de73f6
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-10-21 21:59:54 -0300

    Improved Avatar Component
    Improved Avatar Component
    Improved AvatarMenu component

--- eTasks.Components/Bars/Avatar.razor.cs
+++ eTasks.Components/Bars/Avatar.razor.cs
@@ -11,6 +11,11 @@ namespace eTasks.Components.Bars
         [Parameter] public string OffcanvasMenuID {  get; set; } = string.Empty;
         [Parameter] public EventCallback OnClick { get; set; }
         [Parameter] public bool isDarkMode { get; set; } = false;
+        [Parameter] public int AvatarHeight { get; set; } = 40;
+        [Parameter] public int AvatarWidth { get; set; } = 40;
+        [Parameter] public string AvatarExtraStyle { get; set; } = string.Empty;
+        [Parameter] public string AvatarMarginStyles { get; set; } = "margin-top: 2px;margin-right: 10px;";
+        [Parameter] public bool IsAbleToClick { get; set; } = true;
         #endregion
 
         protected string AvatarImagePath { get; set; } = string.Empty;
--- eTasks.Components/Menus/AvatarMenu.razor.cs
+++ eTasks.Components/Menus/AvatarMenu.razor.cs
@@ -5,5 +5,29 @@ namespace eTasks.Components.Menus
     public class AvatarMenuBase : ComponentBase
     {
         [Parameter] public bool IsDarkMode { get; set; } = false;
+        [Parameter] public string? AvatarPath { get; set; } = string.Empty;
+
+        protected string CloseButton { get; set; } = "assets/UI/offcanvas/light/Close.png";
+        protected string ProfileButton { get; set; } = "assets/UI/avatar/light/EditProfile.png";
+        protected string ChangePasswordButton { get; set; } = "assets/UI/avatar/light/ChangePassword.png";
+        protected string LogoutButton { get; set; } = "assets/UI/avatar/light/Logout.png";
+
+        protected override void OnParametersSet()
+        {
+            if (IsDarkMode)
+            {
+                CloseButton          = "assets/UI/offcanvas/dark/Close.png";
+                ProfileButton        = "assets/UI/avatar/dark/EditProfile.png";
+                ChangePasswordButton = "assets/UI/avatar/dark/ChangePassword.png";
+                LogoutButton         = "assets/UI/avatar/dark/Logout.png";
+            }
+            else
+            {
+                CloseButton          = "assets/UI/offcanvas/light/Close.png";
+                ProfileButton        = "assets/UI/avatar/light/EditProfile.png";
+                ChangePasswordButton = "assets/UI/avatar/light/ChangePassword.png";
+                LogoutButton         = "assets/UI/avatar/light/Logout.png";
+            }
+        }
     }
 }
