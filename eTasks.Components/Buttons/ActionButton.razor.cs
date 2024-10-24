cea9c4e484af6d17f1c906ab84f671618a067b69
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-10-23 22:00:42 -0300

    Implemented Action Button
    Action / Float Button implemented

---
+++ eTasks.Components/Buttons/ActionButton.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Buttons
+{
+    public class ActionButtonBase : ComponentBase
+    {
+        [Parameter] public bool IsDarkMode { get; set; } = false;
+        [Parameter] public EventCallback OnClick { get; set; }
+    }
+}
