fdf95db1df9f2a829ba3643d5170854f20cc1fe6
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-11-02 17:30:03 -0300

    Fix About link
    Fixed About Link
    Tried out PageBase class

--- eTasks.View/Pages/About.razor.cs
+++ eTasks.View/Pages/About.razor.cs
@@ -2,5 +2,13 @@
 {
     public class AboutBase : PageBase
     {
+        protected string Versao { get; set; } = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Versao = "2.0.0";
+        }
     }
 }
