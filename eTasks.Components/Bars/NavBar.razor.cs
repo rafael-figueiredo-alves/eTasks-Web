f9f7b8bfc620de2f770a46d15a025bb98b6aa15a
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-11-04 22:37:35 -0300

    Creating NavBar

---
+++ eTasks.Components/Bars/NavBar.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Bars
+{
+    public class NavBarBase : ComponentBase
+    {
+        [Parameter] public bool IsDarkMode { get; set; } = false;
+    }
+}
