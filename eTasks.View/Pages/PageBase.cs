3ec95aedf682c892003ea00a3ba6c37ac3e3d54b
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-11-11 22:25:16 -0300

    Update PageBase.cs

--- eTasks.View/Pages/PageBase.cs
+++ eTasks.View/Pages/PageBase.cs
@@ -3,23 +3,31 @@ using Microsoft.AspNetCore.Components;
 
 namespace eTasks.View.Pages
 {
+    /// <summary>
+    /// Classe base para Páginas
+    /// </summary>
     public class PageBase : ComponentBase
     {
+        #region Serviços injetados necessários às páginas
         [Inject] public LayoutService? LayoutService { get; set; }
         [Inject] protected NavigationManager? NavigationManager { get; set; }
+        #endregion
 
-        public Type TipoLayout { get; set; } = typeof(eTasks.View.Layouts.MainLayout);
+        #region Variáveis públicas compartilhadas entre telas
+        public Type TipoLayout { get; set; } = typeof(Layouts.MainLayout);
         public bool isMobile { get; set; } = false;
+        #endregion
 
+        #region Métodos
         protected override void OnInitialized()
         {         
             // Define o layout inicial
             isMobile = LayoutService?.IsMobileLayout ?? false;
 
             if (isMobile)
-                TipoLayout = typeof(eTasks.View.Layouts.PageLayout);
+                TipoLayout = typeof(Layouts.PageLayout);
             else
-                TipoLayout = typeof(eTasks.View.Layouts.MainLayout);
+                TipoLayout = typeof(Layouts.MainLayout);
 
             // Inscreve-se para ouvir mudanças no layout
             if (LayoutService != null)
@@ -28,14 +36,18 @@ namespace eTasks.View.Pages
             base.OnInitialized();
         }
 
+        //Método responsáevl por lidar com os layouts
         private void HandleLayoutChanged(bool isMobileLayout)
         {
             isMobile = isMobileLayout;
+
             if (isMobileLayout)
-                TipoLayout = typeof(eTasks.View.Layouts.PageLayout);
+                TipoLayout = typeof(Layouts.PageLayout);
             else
-                TipoLayout = typeof(eTasks.View.Layouts.MainLayout);
+                TipoLayout = typeof(Layouts.MainLayout);
+
             InvokeAsync(StateHasChanged);
         }
+        #endregion
     }
 }
