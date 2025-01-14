0599d4e2ab2b6b2039631d07143e3e2732f4a7af
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-01-13 22:30:41 -0300

    MainMenuItem implemented
    Implemented MainMenu Item Comnponent

---
+++ eTasks.Components/Menus/MainMenuItem.razor.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace eTasks.Components.Menus
+{
+    public class MainMenuItemBase : ComponentBase
+    {
+        #region Parametros
+        [Parameter] public bool IsDarkMode { get; set; } = false;
+        [Parameter] public MainMenuItemType ItemType { get; set; } = MainMenuItemType.Home;
+        [Parameter] public string? ItemName { get; set; } = string.Empty;
+        [Parameter] public bool Selected { get; set; } = false;
+        [Parameter] public EventCallback OnClick { get; set; }
+        #endregion
+
+        #region Variáveis
+        protected string ImageIcon { get; set; } = "assets/UI/mainmenu/light/inicio.png";
+        #endregion
+
+        #region Métodos
+        protected override void OnParametersSet()
+        {
+            var ThemePath = "light";
+            var Item = "conquista";
+
+            if (IsDarkMode)
+            {
+                ThemePath = "dark";
+            }
+
+            switch (ItemType)
+            {
+                case MainMenuItemType.Home:
+                    Item = "inicio";
+                    break;
+                case MainMenuItemType.Tasks:
+                    Item = "tarefas";
+                    break;
+                case MainMenuItemType.Goals:
+                    Item = "metas";
+                    break;
+                case MainMenuItemType.Shopping:
+                    Item = "compras";
+                    break;
+                case MainMenuItemType.Readings:
+                    Item = "leituras";
+                    break;
+                case MainMenuItemType.Notes:
+                    Item = "anotacoes";
+                    break;
+                case MainMenuItemType.Finance:
+                    Item = "financas";
+                    break;
+            }
+
+            ImageIcon = $"assets/UI/mainmenu/{ThemePath}/{Item}.png";
+        }
+        #endregion
+    }
+
+    public enum MainMenuItemType
+    {
+        Home,
+        Tasks,
+        Goals,
+        Shopping,
+        Readings,
+        Notes,
+        Finance
+    }
+}
