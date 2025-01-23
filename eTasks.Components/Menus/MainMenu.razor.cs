f852317cdd789671a25fbeac85b5c6e18022cce6
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-01-23 20:50:13 -0300

    Working on MainMeu
    Removing not used code and adding logic to menu flow

--- eTasks.Components/Menus/MainMenu.razor.cs
+++ eTasks.Components/Menus/MainMenu.razor.cs
@@ -5,8 +5,16 @@ namespace eTasks.Components.Menus
     public class MainMenuBase : ComponentBase
     {
         [Parameter] public bool IsDarkMode { get; set; } = false;
+        
 
         protected string CloseButton { get; set; } = "assets/UI/offcanvas/light/Close.png";
+        protected bool InicioSelected { get; set; } = false;
+        protected bool TarefasSelected { get; set; } = false;
+        protected bool MetasSelected { get; set; } = false;
+        protected bool ComprasSelected { get; set; } = false;
+        protected bool LeiturasSelected { get; set; } = false;
+        protected bool AnotacoesSelected { get; set; } = false;
+        protected bool FinancasSelected { get; set; } = false;
 
         protected override void OnParametersSet()
         {
