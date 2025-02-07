3787feed37025aaac8a305bcf24ee47a5c608e2b
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-02-06 22:32:17 -0300

    Implementing MainMenu

---
+++ eTasks.Shared/Enums/MainMenuItemsEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace eTasks.Shared.Enums
+{
+    public enum MainMenuItemsEnum
+    {
+        Home,
+        Tasks,
+        Goals,
+        Shopping,
+        Readings,
+        Notes,
+        Finances
+    }
+}
