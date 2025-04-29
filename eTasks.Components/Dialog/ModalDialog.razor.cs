24f66ef592e43cc8bb3607df0d32dee7e025491f
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-04-28 22:26:13 -0300

    Created ModalDialog component to test
    Tested Modals from bootstrap with animations on opening and closing.

---
+++ eTasks.Components/Dialog/ModalDialog.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace eTasks.Components.Dialog
+{
+    public class ModalDialogBase : ComponentBase
+    {
+        [Inject] protected IJSRuntime? JSRuntime {  get; set; }
+
+        protected string? ModalId { get; set; } = $"modal-{Guid.NewGuid()}"; 
+
+        public async Task OpenDialog()
+        {
+            await JSRuntime!.InvokeVoidAsync("mostrarModal", ModalId);
+        }
+
+        public async Task CloseModal()
+        {
+            await JSRuntime!.InvokeVoidAsync("fecharModal", ModalId);
+        }
+
+        protected void EscreveMsg()
+        {
+            Console.WriteLine("Clicou em SALVAR");
+        }
+    }
+}
