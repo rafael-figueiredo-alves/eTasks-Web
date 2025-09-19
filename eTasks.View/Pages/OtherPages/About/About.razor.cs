1c873a78c29fe66004bad60ceb8534e00eeab2af
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-09-18 21:27:34 -0300

    Improving About componentization

--- eTasks.View/Pages/OtherPages/About/About.razor.cs
+++ eTasks.View/Pages/OtherPages/About/About.razor.cs
@@ -1,6 +1,7 @@
 ﻿using eTasks.Components;
 using eTasks.Components.Enums;
 using eTasks.Shared.Extensions;
+using eTasks.View.Pages.OtherPages.About.Components;
 
 namespace eTasks.View.Pages.OtherPages.About
 {
@@ -10,10 +11,14 @@ namespace eTasks.View.Pages.OtherPages.About
         protected string CorDestaques { get; set; } = string.Empty;
         protected string CorNormal {  get; set; } = string.Empty;
         protected string Language { get; set; } = string.Empty;
+        protected List<Changelog> Changelogs { get; set; } = new List<Changelog>();
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+
+            Changelogs.Add(new Changelog() { VersionTitle = "14/07/2025 - versão 2.0.1", Features = ["- Implementado recurso Minha Leitura", "- Realizados ajustes gráficos na interface", "- Solucionados bugs no login"] });
+            Changelogs.Add(new Changelog() { VersionTitle = "25/12/2024 - versão 2.0.0", Features = ["- Lançamento da versão 2."] });
 
             await ChangeTheme();
 
@@ -32,6 +37,7 @@ namespace eTasks.View.Pages.OtherPages.About
 
         protected async Task SetCurrentLanguage(string language)
         {
+            await Task.CompletedTask;
             Language = language;
         }
 
---
+++ eTasks.View/Pages/OtherPages/About/Components/Changelog.cs
@@ -0,0 +1,8 @@
+﻿namespace eTasks.View.Pages.OtherPages.About.Components
+{
+    public class Changelog
+    {
+        public string VersionTitle { get; set; } = string.Empty;
+        public List<string> Features { get; set; } = new List<string>();
+    }
+}
