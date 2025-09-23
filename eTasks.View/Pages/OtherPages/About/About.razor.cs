779e2cb16581cb5cbd06bd5aa774867465fb5fe8
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-09-22 21:32:26 -0300

    About improvements

--- eTasks.View/Pages/OtherPages/About/About.razor.cs
+++ eTasks.View/Pages/OtherPages/About/About.razor.cs
@@ -1,24 +1,32 @@
 ﻿using eTasks.Components;
 using eTasks.Components.Enums;
+using eTasks.Shared.Enums;
 using eTasks.Shared.Extensions;
 using eTasks.View.Pages.OtherPages.About.Components;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace eTasks.View.Pages.OtherPages.About
 {
     public class AboutBase : PageBase
-    {   
+    {
+        [Inject]
+        protected HttpClient? Http { get; set; }
+
         protected string Versao { get; set; } = string.Empty;
         protected string CorDestaques { get; set; } = string.Empty;
         protected string CorNormal {  get; set; } = string.Empty;
         protected string Language { get; set; } = string.Empty;
         protected List<Changelog> Changelogs { get; set; } = new List<Changelog>();
+        private List<ChangelogClass> ChangelogClasses { get; set; } = new List<ChangelogClass>();
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
 
-            Changelogs.Add(new Changelog() { VersionTitle = "14/07/2025 - versão 2.0.1", Features = ["- Implementado recurso Minha Leitura", "- Realizados ajustes gráficos na interface", "- Solucionados bugs no login"] });
-            Changelogs.Add(new Changelog() { VersionTitle = "25/12/2024 - versão 2.0.0", Features = ["- Lançamento da versão 2."] });
+            ChangelogClasses = await Http!.GetFromJsonAsync<List<ChangelogClass>>("Changelog.json") ?? new List<ChangelogClass>();
+
+            Changelogs = GetChangeLog(await LanguageService!.GetLanguage());
 
             await ChangeTheme();
 
@@ -39,11 +47,82 @@ namespace eTasks.View.Pages.OtherPages.About
         {
             await Task.CompletedTask;
             Language = language;
+            Changelogs = GetChangeLog(language);
         }
 
         protected void Voltar()
         {
             NavigationManager?.GoBack();
+        }
+
+        private List<Changelog> GetChangeLog(string Language)
+        {
+            var Result = ChangelogClasses.FirstOrDefault(x => x.Language == Language);
+            if (Result == null)
+                return new List<Changelog>();
+
+            return Result.Changelog;
+            //switch (Language)
+            //{
+            //    case LanguagesEnum.PortuguesBrasil:
+            //        return ChangelogInPortuguese();
+            //    case LanguagesEnum.English:
+            //        return ChangelogInEnglish();
+            //    case LanguagesEnum.Espanol:
+            //        throw new NotImplementedException();
+            //    default:
+            //        throw new NotImplementedException();
+            //}
+        }
+
+        private List<Changelog> ChangelogInPortuguese()
+        {
+            return new List<Changelog>()
+            {
+                    new Changelog()
+                    {
+                        VersionTitle = "14/07/2025 - versão 2.0.1",
+                        Features =
+                        [
+                            "- Implementado recurso Minha Leitura",
+                            "- Realizados ajustes gráficos na interface",
+                            "- Solucionados bugs no login"
+                        ]
+                    },
+                    new Changelog()
+                    {
+                        VersionTitle = "25/12/2024 - versão 2.0.0",
+                        Features =
+                        [
+                            "- Lançamento da versão 2."
+                        ]
+                    }
+            };
+        }
+
+        private List<Changelog> ChangelogInEnglish()
+        {
+            return new List<Changelog>()
+            {
+                    new Changelog()
+                    {
+                        VersionTitle = "14/07/2025 - version 2.0.1",
+                        Features =
+                        [
+                            "- New feature: Manage your Readings",
+                            "- Adjustments on user interface",
+                            "- Fixed some bugs on Login"
+                        ]
+                    },
+                    new Changelog()
+                    {
+                        VersionTitle = "25/12/2024 - version 2.0.0",
+                        Features =
+                        [
+                            "- Release of version 2."
+                        ]
+                    }
+            };
         }
     }
 }
--- eTasks.View/Pages/OtherPages/About/Components/Changelog.cs
+++ eTasks.View/Pages/OtherPages/About/Components/Changelog.cs
@@ -5,4 +5,10 @@
         public string VersionTitle { get; set; } = string.Empty;
         public List<string> Features { get; set; } = new List<string>();
     }
+
+    public class ChangelogClass
+    {
+        public string Language { get; set; } = string.Empty;
+        public List<Changelog> Changelog { get; set; } = new List<Changelog>();
+    }
 }
