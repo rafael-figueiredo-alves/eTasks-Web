277b86599e553803adce2c73f98effa8cfc3dd99
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2025-10-30 22:00:02 -0300

    Create VersionInfo.cs

---
+++ eTasks.Model/VersionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace eTasks.Model
+{
+    public class VersionInfo
+    {
+        public int AppVersion { get; set; }
+        public string DisplayVersion { get; set; } = string.Empty;
+        public string URL_APK { get; set; } = string.Empty;
+        public string URL_Win { get; set; } = string.Empty;
+    }
+}
